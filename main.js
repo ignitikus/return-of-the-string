52329b265e74ad214908865cb2f2d37ac1249193
NikolayK <nikolay.kim@codeimmersive.com>
2019-09-26 15:24:24 -0400

    solves all functions

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-function capitalize() {
+function capitalize(str) {
+  return str.toUpperCase(); 
 }
 
-function exclaim() {
+function exclaim(str1) {
+  return str1 + "!";
 }
 
-function firstCharacter() {
+function firstCharacter(str2) {
+  return str2[0];
 }
 
-function lastCharacter() {
+function lastCharacter(str3) {
+  return str3[str3.length - 1];
 }
 
-function oneCharacter() {
+function oneCharacter(str4, char) {
+  return str4[char];
 }
 
-function twoCharacters() {
+function twoCharacters(str5,char1,char2) {
+  return str5[char1]+str5[char2];
 }
 
-function initials() {
+function initials(str6, char1, char2) {
+  let char1 = str6[0];
+  let char2 = str6[str6.indexOf(' ') + 1];
+  return char1 + '.' + char2 + '.';
 }
 
-function yeller() {
+function yeller(str7) {
+  return exclaim(exclaim(exclaim(capitalize(str7))));
 }
 
 
